3160d2a7c13b3ba5d7fec75d9b6e9c9c7bcde118
jianzhi-1 <jianzhiw00@gmail.com>
2021-07-08 23:13:32 +0800

    collector to reject non-creator pixs

--- blockchain_app/nft_module/transactions/transfer_nft_asset.js
+++ blockchain_app/nft_module/transactions/transfer_nft_asset.js
@@ -45,7 +45,23 @@ class TransferNFTAsset extends BaseAsset {
     // 5.verify that the sender owns the nft 
 
     if (!tokenOwnerAddress.equals(senderAddress)) {
-      throw new Error("An NFT can only be transferred by the owner of the NFT.");
+      throw new Error("A Pix can only be transferred by the owner of the Pix.");
+    }
+
+    const recipientAddress = asset.recipient;
+
+    console.log("RECIPIENT ADDRESS")
+    console.log(recipientAddress);
+
+    //if sending to the judge, can only be sent by the creator
+    if (recipientAddress.toString("hex")=="bdcb3fbdfe889dbfaf13c492bd95c44c9ff85177"){
+      if (!tokenOwnerAddress.equals(token.creatorAddress)){
+        throw new Error("A Pix can only be transferred to The Collector by the creator.")
+      } else {
+        console.log("Accepted - sender to the judge is the original creator of the Pix");
+      }
+    } else {
+      console.log("Accepted - recipient is not the judge");
     }
 
     const tokenOwner = await stateStore.account.get(tokenOwnerAddress);
@@ -57,7 +73,7 @@ class TransferNFTAsset extends BaseAsset {
     await stateStore.account.set(tokenOwnerAddress, tokenOwner);
 
     // 7.add nft to the recipient account 
-    const recipientAddress = asset.recipient;
+    
     const recipientAccount = await stateStore.account.get(recipientAddress);
     recipientAccount.nft.ownNFTs.push(token.id);
     await stateStore.account.set(recipientAddress, recipientAccount);
